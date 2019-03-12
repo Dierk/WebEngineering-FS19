434a42a9df16f94cbe7e15ff93f04d7b1e3a48ee
Dierk Koenig <dierk.koenig@canoo.com>
2019-03-12 22:21:52 +0100

    prepare week 4, english class

--- grails-app/controllers/mvc/InPlaceCalculatorController.groovy
+++ grails-app/controllers/mvc/InPlaceCalculatorController.groovy
@@ -8,10 +8,10 @@ class InPlaceCalculatorController {
 //        calcModel.en     = Math.round(calcModel.en   * 10) / 10
 //        calcModel.exam   = Math.round(calcModel.exam * 10) / 10
 //        calcModel.result = Math.round((calcModel.en + calcModel.exam) / 2)
-        if (calcModel.errors.fieldErrors.any { it.field == "en" }) {
-            calcModel.en_error = CalculatorModel.ERROR_CLASS_NAME
-            calcModel.en_error_message = "value '$calcModel.en' is not valid, must be between 1.0 and 6.0."
-        }
+//        if (calcModel.errors.fieldErrors.any { it.field == "en" }) {
+//            calcModel.en_error = "error"
+//            calcModel.en_error_message = "value '$calcModel.en' is not valid, must be between 1.0 and 6.0."
+//        }
 //        if (calcModel.errors.fieldErrors.any { it.field == "exam" }) {
 //            calcModel.exam_error = "error"
 //            calcModel.exam_error_message = "value '$calcModel.exam' is not valid, must be between 1.0 and 6.0."
@@ -25,8 +25,6 @@ class InPlaceCalculatorController {
 }
 
 class CalculatorModel implements Validateable {
-
-    static ERROR_CLASS_NAME = "aöslkfjöas"
 
     double en     = 0.0
     double exam   = 0.0
--- grails-app/views/inPlaceCalculator/calc.gsp
+++ grails-app/views/inPlaceCalculator/calc.gsp
@@ -5,25 +5,12 @@
     <title>
         In-Place Calculator
     </title>
-    <style>
-        .${CalculatorModel.ERROR_CLASS_NAME} {
-            border-color: red;
-        }
-    </style>
 </head>
 
 <body>
 
 <form action="/inPlaceCalculator/calc" method="get">
     <fieldset class="form padded">
-
-        <input type="number" name="en"
-               value="${calculatorInstance.en}"
-               class="${calculatorInstance.en_error}"
-               title="${calculatorInstance.en_error_message}"
-        >
-
-
 
 
         <div>
