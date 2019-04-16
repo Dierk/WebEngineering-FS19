f0cf4f074de5620b579b2424c0f592220c2695a7
Dierk Koenig <dierk.koenig@canoo.com>
2019-04-16 13:36:36 +0200

    week 8: include bookings as REST services

--- grails-app/controllers/mvc/UrlMappings.groovy
+++ grails-app/controllers/mvc/UrlMappings.groovy
@@ -8,7 +8,7 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-
+        "/bookings"(resources:"booking" )
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
--- grails-app/domain/mvc/Booking.groovy
+++ grails-app/domain/mvc/Booking.groovy
@@ -1,5 +1,8 @@
 package mvc
 
+import grails.rest.Resource
+
+@Resource(uri="bookings")
 class Booking {
 
     Room   room
